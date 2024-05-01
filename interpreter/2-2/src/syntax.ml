37fe3963e35ec112c4d394ca8549cb249e44048f
hinata <mikami.hinata.s8@dc.tohoku.ac.jp>
2024-05-01 14:48:44 +0900

    2-2追加

---
+++ interpreter/2-2/src/syntax.ml
@@ -0,0 +1,41 @@
+type name = string 
+
+type literal = 
+  | LInt of int 
+  | LBool of bool 
+
+type binOp = OpAdd | OpSub | OpMul | OpDiv | OpEq | OpLt
+
+type expr =
+  | ELiteral of literal 
+  | EBin of binOp * expr * expr 
+  | EIf of expr * expr * expr 
+  | EVar of name
+  | ELet of name * expr * expr
+  | EFun of name * expr (* fun x -> E*)
+  | EApp of expr * expr (* E E *)
+
+(*env : 変数に値を束縛するリスト型？*)
+and env = (name * value) list
+
+and value =
+  | VInt  of int
+  | VBool of bool 
+  | VFun of name * expr * env
+
+type command =
+  | CExp of expr
+  | CLet of name * expr
+
+
+
+exception Eval_error
+
+let print_value : value -> unit = function 
+  | VInt i -> print_int i 
+  | VBool b -> print_string (string_of_bool b) 
+  | VFun (x, e, env) -> () (*未実装*)
+
+let value_of_literal : literal -> value = function 
+  | LInt i -> VInt i 
+  | LBool b -> VBool b 
