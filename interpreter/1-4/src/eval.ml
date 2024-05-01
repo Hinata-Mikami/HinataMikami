d2bf42b0458ee5c6bf697218c03fe2d5e4549935
hinata <mikami.hinata.s8@dc.tohoku.ac.jp>
2024-05-01 16:27:20 +0900

    eval関数、command関数の型の変更

--- interpreter/1-4/src/eval.ml
+++ interpreter/1-4/src/eval.ml
@@ -1,36 +1,37 @@
 open Syntax
 
-type command_result = Command_result of name option * value * env
+type command_result = Command_result of value * env
 
 (*例外：デバッグ用*)
-exception Eval_error;;
-exception Zero_Division;;
+exception Eval_error
+exception Zero_Division
 exception Unexpected_Expression_at_binOp
 exception Unexpected_Expression_at_eval_if
 exception Variable_Not_Found
 
 (*expr型の解析*)
-let rec eval (env : env) (expr : expr) =
+(*envを返す必要は全くない！！！*)
+let rec eval (env : env) (expr : expr) : value =
   (*binOpの解析*)
   let eval_bin_op (env : env) (op : binOp) (e1 : expr) (e2 : expr) =
     let v1 = eval env e1 in
     let v2 = eval env e2 in
     match v1, v2 with
-    | (VInt x, _), (VInt y, _) ->
+    | VInt x, VInt y ->
     ( match op with
-      | OpAdd -> (VInt (x + y), env)
-      | OpSub -> (VInt (x - y), env)
-      | OpMul -> (VInt (x * y), env)
-      | OpDiv -> if y = 0 then raise Zero_Division else (VInt (x / y), env)
-      | OpEq -> (VBool (x = y), env)
-      | OpLt -> (VBool (x < y), env))
+      | OpAdd -> VInt (x + y)
+      | OpSub -> VInt (x - y)
+      | OpMul -> VInt (x * y)
+      | OpDiv -> if y = 0 then raise Zero_Division else VInt (x / y)
+      | OpEq -> VBool (x = y)
+      | OpLt -> VBool (x < y))
     | _ -> raise Unexpected_Expression_at_binOp
 
   (*ifの解析*)
-  in let eval_if (env : env) (e0 : expr) (e1 : expr) (e2 : expr) =
+  in let eval_if (env : env) (e0 : expr) (e1 : expr) (e2 : expr):value =
     match eval env e0 with
-    | (VBool true, env) -> eval env e1
-    | (VBool false, env) -> eval env e2
+    | VBool true -> eval env e1
+    | VBool false -> eval env e2
     | _ -> raise Unexpected_Expression_at_eval_if
 
   (*Evar n : 変数に束縛された値を返す*)
@@ -44,23 +45,23 @@ let rec eval (env : env) (expr : expr) =
   (*変数の定義*)
   (*let n = e1 in e2 の評価*)
   (*今の環境でe1を評価してその結果をv1とし、その環境でe2を評価する*)
-  in let eval_let (env : env) (n : name) (e1 : expr) (e2 : expr) = 
+  in let eval_let (env : env) (n : name) (e1 : expr) (e2 : expr) : value = 
     match eval env e1 with
-       | (v1,_) -> eval ((n,v1) :: env) e2
+       | v1 -> eval ((n,v1)::env) e2
 
   (*eval関数のメイン部分*)
   in match expr with
-      | ELiteral x -> (value_of_literal x, env)
+      | ELiteral x -> value_of_literal x
       | EBin (op, e1, e2) -> eval_bin_op env op e1 e2
       | EIf (e0, e1, e2) -> eval_if env e0 e1 e2
-      | EVar n -> (lookup_variable env n, env)
+      | EVar n -> lookup_variable env n
       | ELet (x, e1, e2) -> eval_let env x e1 e2
 
 (*次のprint_command_resultで使う*)
 (*CLet (n, e) : let n = e;;*)
-let command_let (env : env) (n : name) (e : expr) =
+let command_let (env : env) (n : name) (e : expr) : (value * env) =
   match eval env e with
-  | (v1,_) -> eval ((n,v1) :: env) e (*eval x 2 になってる*)
+  | v1 -> (v1, ((n,v1) :: env))
 
 
 (*
@@ -77,7 +78,7 @@ let print_command_result (env : env) (cmd : command) : env =
   match cmd with
   | CExp expr -> 
     (match eval env expr with
-     | (v,e) -> print_string (" ― : "); 
+     | v -> print_string (" ― : "); 
      (match v with
      | VInt i -> print_string ("int = "); print_value v; print_newline()
      | VBool b -> print_string ("bool = "); print_value v; print_newline()
