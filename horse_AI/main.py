cee235b8f98400ae08bdc65fbab5419bf92b2ce4
hinata <mikami.hinata.s8@dc.tohoku.ac.jp>
2024-05-21 02:18:39 +0900

    スクレイピング用関数の実装

---
+++ horse_AI/main.py
@@ -0,0 +1,30 @@
+import horseresults_scrape
+import preds_scrape
+import results_scrape
+import return_scrape
+
+race_id_list = []
+for place in range(1, 11, 1):
+    for kai in range(1, 7, 1):
+        for day in range(1, 13, 1):
+            for r in range(1, 13, 1):
+                race_id = "2023" + str(place).zfill(2) + str(kai).zfill(2) + str(day).zfill(2) + str(r).zfill(2)
+                race_id_list.append(race_id)
+
+results = Results.scrape(race_id_list)
+results.to_pickle('results.pickle')
+results = pd.read_pickle('results.pickle')
+
+horse_id_list = results['horse_id'].unique()
+horse_results = HorseResults.scrape(horse_id_list)
+horse_results #jupyterで出力
+
+horse_results.to_pickle('horse_results.pickle')
+
+peds = Peds.scrape(horse_id_list)
+peds
+peds.to_pickle('peds.pickle')
+
+return_tables = Return.scrape(horse_id_list)
+return_tables
+return_tables.to_pickle('return_tables.pickle')
---
+++ horse_AI/update_data.py
@@ -0,0 +1,12 @@
+def update_data(old, new):
+    """
+    Parameters:
+    ----------
+    old : pandas.DataFrame
+        古いデータ
+    new : pandas.DataFrame
+        新しいデータ
+    """
+
+    filtered_old = old[~old.index.isin(new.index)]
+    return pd.concat([filtered_old, new])
