5455a14e217c30f603abd9c21c66ba318b4d8052
hinata <mikami.hinata.s8@dc.tohoku.ac.jp>
2024-10-23 22:13:11 +0900

    更新

--- compiler/step11/config.ml
+++
@@ -1,7 +0,0 @@
-(* configuration *)
-
-let is_macos = false
-
-
-let builddir = "Build"
-let in_build : string -> string = Filename.concat builddir
--- compiler/step11/int.c
+++
@@ -1,8 +0,0 @@
-// Sample function to print an int
-// gcc -O0 -S -fverbose-asm -fomit-frame-pointer
-#include <stdint.h>
-
-extern void print_int(int64_t);
-
-void ti_main(void)
-{ print_int(-42LL); }
