3e741333155868010b4767bb784352b54001d2be
hinata <mikami.hinata.s8@dc.tohoku.ac.jp>
2024-11-05 11:56:21 +0900

    rustコード更新

--- Rust/PythonRust/memory_leak_examples/example_1/src/main.rs
+++ Rust/PythonRust/memory_leak_examples/example_1/src/main.rs
@@ -1,60 +1,53 @@
+use std::cell::RefCell;
 use std::collections::HashMap;
 use std::rc::Rc;
-use std::cell::RefCell;
 
-// ClassBに相当する構造体
 struct ClassB {
-    values: RefCell<HashMap<*const ClassA, String>>, // ポインタ型に変更
+    values: RefCell<HashMap<usize, String>>,
 }
 
 impl ClassB {
     fn new() -> Self {
-        ClassB {
+        Self {
             values: RefCell::new(HashMap::new()),
         }
     }
 
-    fn add(&self, instance: Rc<ClassA>, name: String) {
-        let ptr = Rc::as_ptr(&instance); // ポインタとして格納
-        self.values.borrow_mut().insert(ptr, name);
+    fn add(&self, instance_id: usize, name: String) {
+        self.values.borrow_mut().insert(instance_id, name);
     }
 }
 
-
-#[derive(Clone)]
 struct ClassA {
-    name: String,
     class_b: Rc<ClassB>,
 }
 
 impl ClassA {
-    fn new(class_b: Rc<ClassB>, name: String) -> Self {
-        ClassA { name, class_b }
+    fn new(class_b: Rc<ClassB>) -> Self {
+        Self { class_b }
     }
 
-    fn add(&self) {
-        self.class_b.add(Rc::new(self.clone()), self.name.clone());
+    fn add(&self, name: String) {
+        let instance_id = self as *const _ as usize; // Using the address as a unique identifier
+        self.class_b.add(instance_id, name);
     }
 }
 
-
-// 実行用関数
 fn run(class_b: Rc<ClassB>) {
-    let class_a = Rc::new(ClassA::new(class_b.clone(), String::from("class A")));
-    class_a.add();
-    println!("values: {:?}", class_b.values.borrow());
+    let class_a = ClassA::new(class_b.clone());
+    class_a.add("class A".to_string());
+
+    let values = class_b.values.borrow();
+    println!("values: {:?}", *values);
 }
 
-
-// ループ実行
-fn loop_run() {
+fn loop_fn() {
     let class_b = Rc::new(ClassB::new());
     for _ in 0..3 {
         run(class_b.clone());
     }
 }
 
-
 fn main() {
-    loop_run();
+    loop_fn();
 }
