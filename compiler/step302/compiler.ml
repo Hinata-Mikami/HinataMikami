de5faadc6f82ad8eee9c89edb99d6f277c07d2c7
hinata <mikami.hinata.s8@dc.tohoku.ac.jp>
2024-12-05 09:22:58 +0900

    Ex19

--- compiler/step302/compiler.ml
+++ compiler/step302/compiler.ml
@@ -22,8 +22,8 @@ type atom = C.atom          (* CHANGE *)
 let int  : int64 -> atom = C.int
 let bool : bool -> atom  = C.bool
 
-let var : vname -> atom = fun n -> 
-  if n = "x" then C.varx else Util.fail "Not implemented"
+(* let var : vname -> atom = fun n -> 
+  if n = "x" then C.varx else Util.fail "Not implemented" *)
 
 type repr = C.repr          (* Change *)
 
@@ -38,10 +38,32 @@ let is_zero  : repr -> repr = C.is_zero
 let add : repr -> atom -> repr = C.add
 
 (* Introducing a local variable, and answering questions about it *)
-let local : vname * repr -> repr -> repr = fun (name,exp) body ->
-  Util.fail "Not implemented"
+(* let local : vname * repr -> repr -> repr = fun (name,exp) body ->
+  Util.fail "Not implemented" *)
  
 type obs = C.obs                        (* observation type *)
 
-let observe : repr -> obs = C.observe
+(* let observe : repr -> obs = C.observe *)
+
+
+(* Ex19 *)
+let ty_env : (vname * C.Ty.t) list ref = ref []
+
+let local : vname * repr -> repr -> repr = fun (name, exp ) body ->
+  ty_env := List.remove_assoc name !ty_env; body
+
+let var : vname -> atom = fun n ->
+  match n with
+  | "x" -> C.varx 
+  |  n' -> ty_env := (n', C.Ty.int) :: !ty_env; C.varx
+
+let observe : repr -> obs = fun (ty, seq) ->
+  if !ty_env = [] then
+    Code_gen.observe @@
+    match () with
+    | _ when C.Ty.(eq ty int)  -> Code_gen.call1 "print_int" seq
+    | _ when C.Ty.(eq ty bool) -> Code_gen.call1 "print_bool" seq
+    | _      -> C.Ty.type_error "Unexpected type %s"
+                  (C.Ty.to_string ty)
+  else Util.fail "Unbound variable (observe)"
 
