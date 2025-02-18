9ef7c276918c0fb1eca015524e0edbe04693d88d
hinata <mikami.hinata.s8@dc.tohoku.ac.jp>
2025-02-19 03:03:51 +0900

    スライド作成に向けた修正

--- Rust/Paper/ex_test/src/main.rs
+++ Rust/Paper/ex_test/src/main.rs
@@ -4,25 +4,25 @@
 #[derive(Clone, Debug)] 
 struct Point{ x : i32, y : i32 }
 
+// Drop トレイト
+// デストラクタ実行時に自動で呼ばれる drop メソッドを定義 
 impl Drop for Point{
     fn drop(&mut self) {
-        println!("Dropping Point: x = {}, y = {}", self.x, self.y);
+        println!("Dropping : ({}, {})", self.x, self.y);
     }
 }
 
 
 fn function() {
-    let mut p = Point{x : 12, y : 345};
-    let q = &mut p;
-    *q =  Point{x : 345, y : 12};    // 元の値は drop
-    // p.x = 0;                     // Error
-    // ... q を使用するコード ... 
-    println!("q.x = {}", q.x);      // ライフタイム終了
-    // ... q を使用しないコード ...   // p は使用できる
-    println!("p.x = {}", p.x);
+    let p = Point{x : 12, y : 345};
+    {
+        let q = p;
+    }
+    println!("f2 end");
 }
+
 
 fn main() {
     function();
-    println!("The end of the main function");
+    println!("main end");
 }
--- Rust/Paper/ex_test/target/debug/ex_test.d
+++ Rust/Paper/ex_test/target/debug/ex_test.d
@@ -1,1 +1,1 @@
-C:\Users\mikam\HinataMikami\Rust\Paper\ex_test\target\debug\ex_test.exe: C:\Users\mikam\HinataMikami\Rust\Paper\ex_test\src\main.rs
+/home/hinata/code/HinataMikami/Rust/Paper/ex_test/target/debug/ex_test: /home/hinata/code/HinataMikami/Rust/Paper/ex_test/src/main.rs
